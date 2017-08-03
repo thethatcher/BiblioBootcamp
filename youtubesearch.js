2e3f48a052050ed127a962bb14d54b8b0897fa51
thethatcher <thethatcher@gmail.com>
2017-08-03 10:10:25 -0600

    added the youtubeVid constructor- pushed youtubeVid objects to an array

--- youtubesearch.js
+++ youtubesearch.js
@@ -8,6 +8,7 @@
         ,maxResults: 3
     };
     var videoIds = [];
+    var youtubeVideos = [];
   
     $.getJSON(url, params, function (searchTerm) {
       console.log(searchTerm);
@@ -19,13 +20,19 @@
           $.getJSON(url2 + videoIds[j], params, function(response){
             console.log(response);
             var stats = response.items[0].statistics;
-            console.log("views: " + stats.viewCount);
-            console.log("likes: " + stats.likeCount);
-            console.log("dislikes: " + stats.dislikeCount);
+            youtubeVideos.push(new youtubeVid("test",stats,"test image"));
           })
         }
+        console.log(youtubeVideos);
     });
 
 //TODO create our own youtube video object constructor 
 //add the data we care about- url, stats, like%
+
+function youtubeVid(link,stats,img){
+    this.url = link;
+    this.statistics = stats;
+    this.thumbnail = img;
+    this.statistics.likePercentage = parseInt(this.statistics.likeCount)/(parseInt(this.statistics.likeCount) + parseInt(this.statistics.dislikeCount));
+}
 
