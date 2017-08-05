d10b5eb64f1a31b71e1e47046be43c3fe6f4c3f6
thethatcher <thethatcher@gmail.com>
2017-08-05 11:28:18 -0600

    I think this is the API call that we want.

--- youtubesearch.js
+++ youtubesearch.js
@@ -1,13 +1,13 @@
-    var query = "cool beans";
+    var query = "Jquery getJSON";
     var url = 'https://www.googleapis.com/youtube/v3/search';
-    var url2 = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=';
+    var url2 = 'https://www.googleapis.com/youtube/v3/videos';
     var params = {
         part: 'snippet',
         key: 'AIzaSyCYGQsuv0YLyrE4N11eXj52gBvsnfJ5v9s',
         q: query
         ,maxResults: 3
     };
-
+    var idString = "";
     var youtubeVideos = [];
   
     $.getJSON(url, params, function (searchTerm) {
@@ -19,23 +19,31 @@
             tempVideo.id = searchTerm.items[i].id.videoId;
             tempVideo.thumbnail = searchTerm.items[i].snippet.thumbnails.medium.url;
             tempVideo.description = searchTerm.items[i].snippet.description;
-            
-
-            $.getJSON(url2 + tempVideo.id, params, function(response){
-                // console.log("response: ", response);
-                tempVideo.statistics = response.items[0].statistics;
-                tempVideo.likePercentage = parseInt(tempVideo.statistics.likeCount) / (parseInt(tempVideo.statistics.likeCount) + parseInt(tempVideo.statistics.dislikeCount));
-                console.log("tempVideo: ", tempVideo);
-                youtubeVideos.push(tempVideo);
-            });
-            
+            // console.log("tempVideo: ", tempVideo);   
+            youtubeVideos.push(tempVideo);         
         }
-        console.log(youtubeVideos);
+        getStatistics();
+        console.log("Video Array: ",youtubeVideos);
     });
 
-for (var i = 0; i < youtubeVideos.length; i++) {
-    
+function getStatistics(){
+    for (var i = 0; i < youtubeVideos.length; i++) {
+        idString += (youtubeVideos[i].id + ",");
+    }
+    idString = idString.substring(0,idString.length - 1);
+    console.log("idString: " + idString);
+    var paramVids = {"id" : idString,"part" : "snippet,contentDetails,statistics", key: 'AIzaSyCYGQsuv0YLyrE4N11eXj52gBvsnfJ5v9s'};
+    console.log("getStatistics called");
+    console.log("paramVids: ", paramVids);
+    $.getJSON(url2, paramVids, function(response){
+                console.log("response: ", response);
+                // tempVideo.statistics = response.items[0].statistics;
+                // tempVideo.likePercentage = parseInt(tempVideo.statistics.likeCount) / (parseInt(tempVideo.statistics.likeCount) + parseInt(tempVideo.statistics.dislikeCount));
+                
+                
+            });
 }
+
 
     
 
