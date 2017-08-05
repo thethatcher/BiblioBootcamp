6a3c9a1ad8acf7adf3b8d402fa1c981418bcf67a
thethatcher <thethatcher@gmail.com>
2017-08-05 10:08:49 -0600

    asynchronous functions are causing problems

--- youtubesearch.js
+++ youtubesearch.js
@@ -14,34 +14,37 @@
       console.log(searchTerm);
         for (var i = 0; i < searchTerm.items.length; i++) {
             youtubeVideos.push(new youtubeVid());
-            console.log("https://www.youtube.com/watch?v=" + searchTerm.items[i].id.videoId);
+
             youtubeVideos[i].url = "https://www.youtube.com/watch?v=" + searchTerm.items[i].id.videoId;
+            youtubeVideos[i].id = searchTerm.items[i].id.videoId;
             youtubeVideos[i].thumbnail = searchTerm.items[i].snippet.thumbnails.medium.url;
             youtubeVideos[i].description = searchTerm.items[i].snippet.description;
             console.log("object: ", youtubeVideos[i]);
 
             //calling youtube API to get specific video statistics
+            $.getJSON(url2 + youtubeVideos[i].id, params, function(response){
+                console.log("response: ", response);
+                // youtubeVideos[i].statistics = response.items[0].statistics;
+                // youtubeVideos[i].likePercentage = parseInt(youtubeVideos[i].statistics.likeCount) / (parseInt(youtubeVideos[i].statistics.likeCount) + parseInt(youtubeVideos[i].statistics.dislikeCount));
+            });
         }
 
         console.log(youtubeVideos);
     });
 
     
-for (var i = 0; i < youtubeVideos.length; i++) {
-    $.getJSON(youtubeVideos[i].url, params, function(response){
-                console.log("response: ", response);
-                var stats = response.items[0].statistics;
-            });
-}
+
     
 
 //TODO create our own youtube video object constructor 
 //add the data we care about- url, stats, like%
 
 function youtubeVid(){
+    this.id ;
     this.url ;
-    this.statistics ;
-    this.likePercentage ;
     this.thumbnail ;
     this.description ;
+    this.statistics ;
+    this.likePercentage ;
+    
 }
