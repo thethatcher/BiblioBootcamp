180906f1af1458b675118a34a0fec30f5069e5a6
thethatcher <thethatcher@gmail.com>
2017-08-05 12:53:24 -0600

    moved youtube.js to the app.js starting to connect to the front end

--- youtubesearch.js
+++ youtubesearch.js
@@ -1,56 +1,52 @@
-    var query = "Jquery getJSON";
-    var url = 'https://www.googleapis.com/youtube/v3/search';
-    var url2 = 'https://www.googleapis.com/youtube/v3/videos';
-    var params = {
-        part: 'snippet',
-        key: 'AIzaSyCYGQsuv0YLyrE4N11eXj52gBvsnfJ5v9s',
-        q: query
-        ,maxResults: 3
-    };
-    var idString = "";
-    var youtubeVideos = [];
+var query = "Jquery getJSON";
+var url = 'https://www.googleapis.com/youtube/v3/search';
+var url2 = 'https://www.googleapis.com/youtube/v3/videos';
+var params = {
+    part: 'snippet',
+    key: 'AIzaSyCYGQsuv0YLyrE4N11eXj52gBvsnfJ5v9s',
+    q: query
+    ,maxResults: 50
+};
+var idString = "";
+var youtubeVideos = [];
   
-    $.getJSON(url, params, function (searchTerm) {
-      console.log(searchTerm);
-        for (var i = 0; i < searchTerm.items.length; i++) {
-            var tempVideo = new youtubeVid();
-
-            tempVideo.url = "https://www.youtube.com/watch?v=" + searchTerm.items[i].id.videoId;
-            tempVideo.id = searchTerm.items[i].id.videoId;
-            tempVideo.thumbnail = searchTerm.items[i].snippet.thumbnails.medium.url;
-            tempVideo.description = searchTerm.items[i].snippet.description;
-            // console.log("tempVideo: ", tempVideo);   
-            youtubeVideos.push(tempVideo);         
-        }
-        getStatistics();
-        console.log("Video Array: ",youtubeVideos);
-    });
+$.getJSON(url, params, function (searchTerm) {
+  console.log(searchTerm);
+    for (var i = 0; i < searchTerm.items.length; i++) {
+        idString += (searchTerm.items[i].id.videoId + ",");        
+    }
+    getStatistics();
+});
 
 function getStatistics(){
-    for (var i = 0; i < youtubeVideos.length; i++) {
-        idString += (youtubeVideos[i].id + ",");
-    }
     idString = idString.substring(0,idString.length - 1);
     console.log("idString: " + idString);
-    var paramVids = {"id" : idString,"part" : "snippet,contentDetails,statistics", key: 'AIzaSyCYGQsuv0YLyrE4N11eXj52gBvsnfJ5v9s'};
+    var paramVids = {
+        "id" : idString
+        ,"part" : "snippet,contentDetails,statistics"
+        , "key": 'AIzaSyCYGQsuv0YLyrE4N11eXj52gBvsnfJ5v9s'
+    };
     console.log("getStatistics called");
-    console.log("paramVids: ", paramVids);
     $.getJSON(url2, paramVids, function(response){
-                console.log("response: ", response);
-                // tempVideo.statistics = response.items[0].statistics;
-                // tempVideo.likePercentage = parseInt(tempVideo.statistics.likeCount) / (parseInt(tempVideo.statistics.likeCount) + parseInt(tempVideo.statistics.dislikeCount));
-                
-                
-            });
+        console.log("response: ", response);
+        for (var i = 0; i < response.items.length; i++) {
+            var tempVideo = new youtubeVid();
+            tempVideo.id = response.items[i].id;
+            tempVideo.url = "https://www.youtube.com/watch?v=" + tempVideo.id;
+            tempVideo.thumbnail = response.items[i].snippet.thumbnails.medium.url;
+            tempVideo.description = response.items[i].snippet.description;
+            tempVideo.statistics = response.items[i].statistics;
+            tempVideo.likePercentage = parseInt(tempVideo.statistics.likeCount) / (parseInt(tempVideo.statistics.likeCount) + parseInt(tempVideo.statistics.dislikeCount));
+            tempVideo.title = response.items[i].snippet.title;
+            youtubeVideos.push(tempVideo);
+        }
+        listResults(youtubeVideos);
+    });
 }
 
-
-    
-
-    
-
-//TODO create our own youtube video object constructor 
-//add the data we care about- url, stats, like%
+function listResults(array){
+    console.log("Video Array: ", array);
+}
 
 function youtubeVid(){
     this.id ;
@@ -59,5 +55,5 @@ function youtubeVid(){
     this.description ;
     this.statistics ;
     this.likePercentage ;
-    
+    this.title;
 }
