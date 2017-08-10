2cd84f6ff7d4c2a5db3d473f67462b7ef7681255
MICHAEL HEDIN <MICHAEL HEDIN>
2017-08-09 19:32:50 -0600

    link target blank

--- js/TestSearch.js
+++ js/TestSearch.js
@@ -18,12 +18,13 @@ var queryURL="https://www.googleapis.com/customsearch/v1?q="+SearchTerm+"&cx=004
         }).done(function(response) {
           console.log(response);
 
+
           for (var i = 0; i < response.items.length; i++) {
 
           	
           
-          var content = "<div id='contentItem'><h3>" + "<a href=" + response.items[i].formattedUrl+">" + response.items[i].title + "</a>" + "</h3>" + "<p>Section: "+ response.items[i].snippet + "</p></div>";
-
+          var content = "<div class='contentItem'><h3>" + "<a href='" + response.items[i].formattedUrl+"'target='_blank'>" + response.items[i].title + "</a>" + "</h3>" + "<p>"+ response.items[i].snippet + "</p></div>";
+          console.log(content);
           $('#empty-div').append(content);
       		}
 
@@ -39,7 +40,8 @@ $('#empty-div').empty();
 var SearchTerm = $("#search").val().trim();
 
 console.log ("Search Term: " + SearchTerm);
-//Overstack.com
+
+//Stackoverflow
 var query2URL="https://www.googleapis.com/customsearch/v1?q="+SearchTerm+"&cx=004474692957123199963:zwg44jgvva0&key=AIzaSyAXET23jWz1E-N-JeBJ-3rGq8oqQ-Cy9gc"
 
   $.ajax({
@@ -52,7 +54,7 @@ var query2URL="https://www.googleapis.com/customsearch/v1?q="+SearchTerm+"&cx=00
 
           	
           
-          var content2 = "<div id='contentItem'><h1>" + response2.items[i].title + "</h1> <a href=" + response2.items[i].link + ">"+ response2.items[i].link+ "</a>"  + "</h1> <p>Section: "+ response2.items[i].snippet + "</p></div>";
+          var content2 = "<div class='contentItem'><h3>" +  "<a href='" + response2.items[i].link + "'target='_blank'>"+ response2.items[i].title+ "</a>"  + "</h1> <p>"+ response2.items[i].snippet + "</p></div>";
 
           $('#empty-div').append(content2);
       		}
@@ -79,7 +81,7 @@ $("#googleSearchButton").on("click",function(event){
 
           	
           
-          var content3 = "<div id='contentItem'><h1>" + response3.items[i].title + "</h1> <a href=" + response3.items[i].link + ">"+ response3.items[i].link+ "</a>"  + "</h1> <p>Section: "+ response3.items[i].snippet + "</p></div>";
+          var content3 = "<div class='contentItem'><h3>" +  "<a href='" + response3.items[i].link + "'target='_blank'>"+ response3.items[i].title+ "</a>"  + "</h1> <p>"+ response3.items[i].snippet + "</p></div>";
 
           $('#empty-div').append(content3);
       }
